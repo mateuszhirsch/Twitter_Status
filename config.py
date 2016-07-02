7e995ed323ab5f993ea3054277b7419966a2e478
Mateusz Hirsch <mateuszhirsch@gmail.com>
2016-07-02 21:27:48 +0200

    Add twitter config file

---
+++ config.py
@@ -0,0 +1,10 @@
+#--------------------------------------------------------------------------------
+# These tokens are needed for user authentication.
+# Credentials can be generates via Twitter's Application Management:
+# https://apps.twitter.com/app/new
+#--------------------------------------------------------------------------------
+
+consumer_key = "wFlgstVDkpsM1N6TdjYlw41dZ"
+consumer_secret = "ivizD0XP1YOMf5hbqSLSTi861iSriBMXgJ9V8s5GZyomySyxSG"
+access_key = "749277828684902400-VT1925eUE4OPWKQJWOdwxVtNlx9CUPD"
+access_secret = "ATB8NoH839gqUm7jJTt3e7yGsBzG2O7xwlvrhBFyTCBwy"
